72d92a5d0a38583d308073bf952f356667237ca6
zinuok <45934290+zinuok@users.noreply.github.com>
2020-07-23 18:52:52 +0900

    Update install_etc.sh

--- install_etc.sh
+++ install_etc.sh
@@ -3,7 +3,7 @@ echo "install chrome start"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
-sudo apt-get install google-chrome-stable
+sudo apt install -y google-chrome-stable
 sudo rm -rf /etc/apt/sources.list.d/google.list
 
 # install terminator
@@ -12,7 +12,7 @@ sudo apt install -y terminator
 
 # install visual studio code
 echo "install vscode start"
-sudo apt install curl
+sudo apt install -y curl
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
