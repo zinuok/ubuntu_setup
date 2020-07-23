64b14dcdf0332316340c0efa047297e0f6217aa8
zinuok <45934290+zinuok@users.noreply.github.com>
2020-07-23 18:32:39 +0900

    Update install_etc.sh

--- install_etc.sh
+++ install_etc.sh
@@ -1,4 +1,12 @@
 # install terminator
+echo "install chrome start"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt-get install google-chrome-stable
+sudo rm -rf /etc/apt/sources.list.d/google.list
+
+# install terminator
 echo "install terminator start"
 sudo apt install -y terminator
 
