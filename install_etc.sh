b2d4045583712206e2db92728aec17bb005d9c3d
zinuok <45934290+zinuok@users.noreply.github.com>
2020-07-23 18:30:40 +0900

    Update install_etc.sh

--- install_etc.sh
+++ install_etc.sh
@@ -1,6 +1,6 @@
 # install terminator
 echo "install terminator start"
-sudo apt install terminator
+sudo apt install -y terminator
 
 # install visual studio code
 echo "install vscode start"
@@ -8,4 +8,4 @@ sudo apt install curl
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
-sudo apt install code
+sudo apt install -y code
