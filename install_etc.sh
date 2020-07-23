76c2fd34853fa53299203fc8916de5adc31703b4
zinuok <45934290+zinuok@users.noreply.github.com>
2020-07-23 09:22:17 +0000

    Add files via upload

---
+++ install_etc.sh
@@ -0,0 +1,11 @@
+# install terminator
+echo "install terminator start"
+sudo apt install terminator
+
+# install visual studio code
+echo "install vscode start"
+sudo apt install curl
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
